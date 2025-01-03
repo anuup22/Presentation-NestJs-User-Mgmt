e3e8f7193cc332f80559cf74ab468edff6a6794b
Anup Gupta <anup.gupta@nonstopio.com>
2025-01-03 11:26:34 +0530

    feat: add logging middleware to log requests and configure it in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { LoggingMiddleware } from './logging/logging.middleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*'); // Apply to all routes
+  }
+}
