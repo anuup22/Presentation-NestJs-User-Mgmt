7f9d7b68ecc85354815cee29992f393467601daf
Anup Gupta <anup.gupta@nonstopio.com>
2025-01-03 11:35:16 +0530

    feat: add CustomExceptionFilter for handling HTTP exceptions and integrate it into the application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform/transform.interceptor';
+import { CustomExceptionFilter } from './custom-exception/custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new CustomExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
